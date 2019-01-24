2f6c417e3ab2d681520c6d54a65092810ddc1093
kaichiro <kcfg23@gmail.com>
2019-01-24 20:05:53 -0200

    Removendo arquivos desnecessários

--- src/props/CarrinhoDeCompras.tsx
+++
@@ -1,9 +0,0 @@
-import CardComponentProps from "./CardComponentProps";
-
-// tslint:disable-next-line:interface-name
-interface CarrinhoDeCompras {
-  quantidade: number;
-  prods: CardComponentProps[];
-}
-
-export default CarrinhoDeCompras;
